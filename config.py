168d36a7afe95ee82ebbf81caf7c65f07c89de50
m7hmd <102188298+m7hmd@users.noreply.github.com>
2022-04-12 14:00:29 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "5285309268:AAHHPhigkibAd57942s_QsBnoAWMikAdRWE"
+BOT_TOKEN = "5295940154:AAGZc95-F6VltntXMGDGIsyRb21qI8EIZwQ"
 APP_URL = "https://temidolebot.herokuapp.com/" + BOT_TOKEN
